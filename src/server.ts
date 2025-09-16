1a0a601a7c79eb1f1154b98d14c508c14d2208cf
HenriqueGennari <henriquegennarilemgruber@gmail.com>
2025-09-16 14:18:51 -0300

    Ajustando peças fundamentais

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv"
+
+dotenv.config();
+
+const server = express();
+
+server.listen(process.env.PORT, () => {
+    console.log(`Server rodando na porta ${process.env.PORT}`)
+})
