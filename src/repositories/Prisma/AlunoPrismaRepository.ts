d5bf68668da294ce526f1cbb10a980a4f877be04
HenriqueGennari <henriquegennarilemgruber@gmail.com>
2025-09-25 21:29:31 -0300

    Atualizando arquivos para funcionamento do render

--- src/repositories/Prisma/AlunoPrismaRepository.ts
+++ src/repositories/Prisma/AlunoPrismaRepository.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import type { Alunos } from "../../models/Alunos";
 
 const prisma = new PrismaClient()
